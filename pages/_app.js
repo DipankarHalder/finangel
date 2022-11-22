cb6dcde0fbe61b96a0e8cfeddc2637a9092ce14f
Dipankar Halder <webmail.dip@gmail.com>
2022-11-23 01:11:01 +0530

    completed setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "@/root/globals.scss";
+import Head from "next/head";
+import { Fragment } from "react";
+import MainLayout from "@/mainLayer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || (page => page);
+
+  return getLayout(
+    <Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
