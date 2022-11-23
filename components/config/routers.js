81ace0b281dcaf92ef1290e89c2c38e261339eb0
Dipankar Halder <webmail.dip@gmail.com>
2022-11-23 17:48:24 +0530

    added router and others

---
+++ components/config/routers.js
@@ -0,0 +1,6 @@
+export const HOME = "/";
+export const ABOUT = "/about";
+export const MISSION = "/mission";
+export const STORES = "/stores";
+export const COMMUNITY = "/community";
+export const LOGIN = "/login";
